0cb9cd745e38e3a96d4ed4ce9d18e1137d362246
Estela Real <estelaretin10@gmail.com>
2025-11-06 15:39:06 +0100

    cambios para despliegue en produccion

--- servidor/passport-setup.js
+++ servidor/passport-setup.js
@@ -2,10 +2,11 @@ const passport=require("passport");
 const GoogleStrategy = require('passport-google-oauth20').Strategy; 
 const GoogleOneTapStrategy = require("passport-google-one-tap").GoogleOneTapStrategy;
 
+const CALLBACK_BASE_URL = process.env.BASE_URL || "http://localhost:3000";
 passport.serializeUser(function(user, done) { done(null, user); }); 
 passport.deserializeUser(function(user, done) { done(null, user); }); 
 passport.use(new GoogleStrategy({ clientID: process.env.GOOGLE_CLIENT_ID,
-     clientSecret: process.env.GOOGLE_CLIENT_SECRET, callbackURL: "http://localhost:3000/google/callback" },
+     clientSecret: process.env.GOOGLE_CLIENT_SECRET, callbackURL: `${CALLBACK_BASE_URL}/google/callback` },
      function(accessToken, refreshToken, profile, done) { return done(null, profile); } ));
 
      passport.use( 
